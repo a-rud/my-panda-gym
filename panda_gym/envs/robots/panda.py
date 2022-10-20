12a69a4b4005093ba705b730e2a877c6399c3a05
Adrian Rudloff <80639001+a-rud@users.noreply.github.com>
2022-10-21 00:28:15 +0200

    - Add redundancy resolution and Jacobian for Panda robot - Add PyBullet functionality to calculate Jacobian - Add multiple low-level PyBullet functions: get_link_state, get_joint_states, velocity_control_joints

--- panda_gym/envs/robots/panda.py
+++ panda_gym/envs/robots/panda.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import numpy as np
 from gym import spaces
 
@@ -24,11 +25,14 @@ class Panda(PyBulletRobot):
             control_type: str = "ee",
             velocity_control: bool = False,
             redundancy_resolution: bool = False,
+            redundancy_resolution_scaling: float = 1.0
     ) -> None:
         self.block_gripper = block_gripper
         self.control_type = control_type
         self.velocity_control = velocity_control
         self.use_redundancy_resolution = redundancy_resolution
+        self.redundancy_resolution_scaling = redundancy_resolution_scaling
+        self.num_joints_arm = 7
         n_action = 3 if self.control_type == "ee" else 7  # control (x, y z) if "ee", else, control the 7 joints
         n_action += 0 if self.block_gripper else 1
         action_space = spaces.Box(-1.0, 1.0, shape=(n_action,), dtype=np.float32)
@@ -57,10 +61,13 @@ class Panda(PyBulletRobot):
             if self.control_type == "ee":
                 raise NotImplementedError("Velocity control is not supported in EE control atm.")
             else:
-                arm_joint_ctrl = action[:7]
+                arm_joint_ctrl = action[:self.num_joints_arm]
                 if self.use_redundancy_resolution:
-                    raise NotImplementedError("Redundancy resolution not yet implemented")
-                # target_velocities = self.arm_joint_ctrl_to_target_arm_angles(arm_joint_ctrl)
+                    qdot_nullspace = self.resolve_redundancy(
+                        q_reference=self.neutral_joint_values[:self.num_joints_arm],
+                        scaling_factor=self.redundancy_resolution_scaling
+                    )
+                    arm_joint_ctrl = arm_joint_ctrl + qdot_nullspace[:self.num_joints_arm]
 
             if self.block_gripper:
                 fingers_ctrl = -0.01
@@ -157,128 +164,67 @@ class Panda(PyBulletRobot):
         """Returns the velocity of the end-effector as (vx, vy, vz)"""
         return self.get_link_velocity(self.ee_link)
 
-    # def get_jacobian(self):
-    #     """
-    #     This function is very closely adapted from the PyBullet examples. Can be found under
-    #     https://github.com/bulletphysics/bullet3/blob/master/examples/pybullet/examples/jacobian.py
-    #     Inside the Bullet repository: bullet3/examples/pybullet/examples/jacobian.py
-    #
-    #     Returns:
-    #
-    #     """
-    #     num_joints = 7
-    #     assert isinstance(self.sim, PyBullet), f"Simulation must be type PyBullet but got f{type(self.sim)} instead."
-    #
-    #     def getMotorJointStates(robot):
-    #         """
-    #         getJointStates output:
-    #         jointPosition:              The position value of this joint.
-    #         jointVelocity:              The velocity value of this joint.
-    #         jointReactionForces:        These are the joint reaction forces, if a torque sensor is enabled for this joint it
-    #                                     is [Fx, Fy, Fz, Mx, My, Mz]. Without torque sensor, it is [0,0,0,0,0,0].
-    #         appliedJointMotorTorque:    This is the motor torque applied during the last stepSimulation
-    #         """
-    #         joint_states_all = self.sim.physics_client.getJointStates(robot, range(num_joints))
-    #         """
-    #         getJointInfo output:
-    #         jointIndex:         the same joint index as the input parameter
-    #         jointName:          the name of the joint, as specified in the URDF (or SDF etc) file
-    #         jointType:          type of the joint, this also implies the number of position and velocity variables.
-    #         qIndex:             the first position index in the positional state variables for this body
-    #         uIndex:             the first velocity index in the velocity state variables for this body
-    #         """
-    #         joint_infos = [self.sim.physics_client.getJointInfo(robot, i) for i in range(num_joints)]
-    #         joint_states = [j for j, i in zip(joint_states_all, joint_infos) if i[3] > -1]
-    #         joint_positions = [state[0] for state in joint_states]
-    #         joint_velocities = [state[1] for state in joint_states]
-    #         joint_torques = [state[3] for state in joint_states]
-    #         return joint_positions, joint_velocities, joint_torques
-    #
-    #     def multiplyJacobian3D(robot, jacobian, vector):
-    #         result = [0.0, 0.0, 0.0]
-    #         i = 0
-    #         joint_infos = [p.getJointInfo(robot, i) for i in range(len(vector))]
-    #         for c in range(len(vector)):
-    #             info = joint_infos[c]
-    #             if info[3] > -1:
-    #                 for r in range(3):
-    #                     result[r] += jacobian[r][i] * vector[c]
-    #                 i += 1
-    #         return result
-    #
-    #     # Get the joint and link state directly from Bullet.
-    #     pos, vel, torq = getJointStates(kukaId)
-    #     mpos, mvel, mtorq = getMotorJointStates(kukaId)
-    #
-    #     zero_vec = [0.0] * len(mpos)
-    #     jac_t, jac_r = p.calculateJacobian(
-    #         bodyUniqueId=kukaId,
-    #         linkIndex=kukaEndEffectorIndex,
-    #         localPosition=com_trn,
-    #         objPositions=mpos,
-    #         objVelocities=zero_vec,
-    #         objAccelerations=zero_vec
-    #     )
-    #
-    #     # Motor state
-    #     mpos, mvel, mtorq = getMotorJointStates(self.sim._bodies_idx[self.body_name])
-    #
-    #     """
-    #         FROM PYBULLET DOC:
-    #     position_linkcom_world, world_rotation_linkcom,
-    #     position_linkcom_frame, frame_rotation_linkcom,
-    #     position_frame_world, world_rotation_frame,
-    #     linearVelocity_linkcom_world, angularVelocity_linkcom_world
-    #       = getLinkState(objectUniqueId, linkIndex, computeLinkVelocity=0,
-    #                      computeForwardKinematics=0, physicsClientId=0)
-    #     Provides extra information such as the Cartesian world coordinates center of mass (COM) of the link, relative to the world reference frame.
-    #     """
-    #     linkstate = self.sim.physics_client.getLinkState(
-    #         bodyUniqueId=self.sim._bodies_idx[self.body_name],
-    #         linkIndex=self.ee_link,
-    #         computeLinkVelocity=1,
-    #         computeForwardKinematics=1
-    #     )
-    #     link_trn, link_rot, com_trn, com_rot, frame_pos, frame_rot, link_vt, link_vr = linkstate
-    #     """
-    #         FROM QUICKSTART GUIDE:
-    #     link_trn    =   linkWorldPosition               (Cartesian position of center of mass)
-    #     link_rot    =   linkWorldOrientation            (Cartesian orientation of center of mass, in
-    #                                                     quaternion [x,y,z,w])
-    #     com_trn     =   localInertialFramePosition      (local position offset of inertial frame (center of mass)
-    #                                                     expressed in the URDF link frame)
-    #     com_rot     =   localInertialFrameOrientation   (local orientation (quaternion [x,y,z,w]) offset of the inertial
-    #                                                     frame expressed in URDF link frame.)
-    #     frame_pos   =   worldLinkFramePosition          (world position of the URDF link frame)
-    #     frame_rot   =   worldLinkFrameOrientation       (world orientation of the URDF link frame)
-    #     link_vt     =   worldLinkLinearVelocity         (Cartesian world velocity. Only returned if computeLinkVelocity
-    #                                                     non-zero)
-    #     link_vr     =   worldLinkAngularVelocity        (Cartesian world velocity. Only returned if computeLinkVelocity
-    #                                                     non-zero)
-    #
-    #     """
-    #
-    #     """
-    #     calculateJacobian requires:
-    #
-    #     bodyUniqueId int                body unique id, as returned by loadURDF etc.
-    #     linkIndex int                   link index for the jacobian.
-    #     localPosition list of float     the point on the specified link to compute the jacobian for, in
-    #                                     link local coordinates around its center of mass.
-    #     objPositions list of float      joint positions (angles)
-    #     objVelocities list of float     joint velocities
-    #     objAccelerations list of float  desired joint accelerations
-    #     """
-    #     # Get the Jacobians for the CoM of the end-effector link.
-    #     # Note that in this example com_rot = identity, and we would need to use com_rot.T * com_trn.
-    #     # The localPosition is always defined in terms of the link frame coordinates.
-    #     J = self.sim.physics_client.calculateJacobian(
-    #         bodyUniqueId=self.sim._bodies_idx[self.body_name],
-    #         linkIndex=self.ee_link,
-    #         localPosition=com_trn,
-    #         objPositions=mpos,
-    #         objVelocities=zero_vec,
-    #         objAccelerations=zero_vec
-    #     )
-    #     J_trans, J_rot = J
-    #     return J
+    def get_jacobian(self) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        This function is very closely adapted from the PyBullet examples. Can be found under
+        https://github.com/bulletphysics/bullet3/blob/master/examples/pybullet/examples/jacobian.py
+        Inside the Bullet repository: bullet3/examples/pybullet/examples/jacobian.py
+        """
+        """
+        calculateJacobian requires:
+
+        bodyUniqueId int                body unique id, as returned by loadURDF etc.
+        linkIndex int                   link index for the jacobian.
+        localPosition list of float     the point on the specified link to compute the jacobian for, in
+                                        link local coordinates around its center of mass.
+        objPositions list of float      joint positions (angles)
+        objVelocities list of float     joint velocities
+        objAccelerations list of float  desired joint accelerations
+        """
+        # Robot's moto states:
+        mpos, _, _ = self.sim.get_motor_joint_states(body=self.body_name, joints=list(self.joint_indices))
+        # State of EE link:
+        _, _, com_trn, _, _, _, _, _ = self.sim.get_link_state(body=self.body_name, link=self.ee_link)
+        zero_vec = np.zeros(len(mpos))
+
+        # Get the Jacobians for the CoM of the end-effector link.
+        # Note that in this example com_rot = identity, and we would need to use com_rot.T * com_trn.
+        # The localPosition is always defined in terms of the link frame coordinates.
+        J_trans, J_rot = self.sim.calculate_jacobian(
+            body=self.body_name,
+            link=self.ee_link,
+            local_position=list(com_trn),
+            obj_positions=list(mpos),
+            obj_velocities=list(zero_vec),
+            obj_accelerations=list(zero_vec)
+        )
+        return J_trans, J_rot
+
+    def resolve_redundancy(self, q_reference: np.ndarray, scaling_factor: float = 1.0) -> np.ndarray:
+        """
+        Resolve redundancy be generating a nullspace vector (joint velocity in Jacobians nullspace) to move
+        the robot towards a reference config without moving the EE POSITION (orientation DOES change).
+        Args:
+            q_reference: 7 dimensional vector of desired joint configuration
+
+        Returns:
+            qdot_nullspace: joint velocity vector in nullspace. This vector can be added to
+        """
+
+        J_trans, J_rot = self.get_jacobian()
+        J_trans = J_trans[:, :self.num_joints_arm]
+
+        # Calculate qdot_nullspace = -scaling * (I-J^+*J)b
+        J_plus = np.linalg.pinv(J_trans)
+        nullspace_projector = np.identity(self.num_joints_arm) - np.matmul(J_plus, J_trans)
+
+        # current joint config q:
+        q = np.array([self.get_joint_angle(i) for i in self.joint_indices[:self.num_joints_arm]])
+        assert q.shape == q_reference.shape, \
+            f"Panda arm config and reference joint config shapes do not match: {q.shape} != {q_reference.shape}"
+        # error vector between current joint config and desired config
+        e = q - q_reference
+
+        # project error vector into nullspace
+        q_dot_nullspace = - scaling_factor * np.matmul(nullspace_projector, e)
+        return q_dot_nullspace
